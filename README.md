c2f0d343fe790c8b671845e737bc17c2c707a531
Nahid <nahidhassan25@gmail.com>
2021-03-13 16:39:06 -0500

    dice roller, but now with images

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Dice-Roller
 Learning android, following https://developer.android.com/courses/pathways/android-basics-kotlin-four
+
+Build using Android studios.
+Minimum android version: 19
--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,32 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Set static roll value
-        /*
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-        }
-        */
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        // Set the initial dice image to 1
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_1)
+        diceImage.contentDescription = "1"
 
+        // Button to change image
+        val rollButton: Button = findViewById(R.id.button)
+        // On button click, change image
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    // Roll dice and show images
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
 class Dice(val numSides: Int) {
-
+    // Random number generator from 1 to 6
     fun roll(): Int {
         return (1..numSides).random()
     }
